77fe9a740ec4eb7989df3a83b68e95c3f158d731
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-28 21:51:08 -0800

    Create Header component

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React from "react"
 import NavBar from "./components/NavBar"
+import Header from "./components/Header"
 
 function App() {
   return (
-
+<div>
 <NavBar/>
+<Header/>
 
+</div>
   )
 }
 
---
+++ react-portfolio/src/components/Header/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./style.css";
+// import NavBar from "./components/NavBar"
+
+function Header() {
+  return (
+    <section class="hero is-fullheight is-dark" id="content1">
+      <div class="hero-head">
+      
+
+      </div>
+
+      {/* // <!-- Hero content: will be in the middle --> */}
+
+      <div class="hero-body" id="content1">
+        <div class="container has-text-centered ">
+          <div class="column is-one-third" id="intro">
+            <h2 class="subtitle has-text-white">Online Portfolio</h2>
+            <h1 class="title has-text-white">RON-ARJAY CALUAG</h1>
+            <h2 class="has-text-white"> Full Stack Web Developer</h2>
+
+            <a href="#about-me-sec" class="button mt-4">
+              About Me
+            </a>
+            <a href="#skillsSec" class="button mt-4">
+              Skill Set
+            </a>
+            <br />
+            <a href="#Projects" class="button mt-4">
+              Projects
+            </a>
+            <a href="assets/Ron-ArjayCaluagBerResume.pdf" class="button mt-4">
+              Resume
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
