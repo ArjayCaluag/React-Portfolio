e861d2a61c4a6051c918d9942c65e449afe4a1b6
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-28 21:06:28 -0800

    Create NavBar component

---
+++ react-portfolio/src/components/NavBar/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./style.css";
+
+function NavBar() {
+  return (
+    <nav class="navbar" role="navigation" aria-label="main navigation" id="navbar1">
+                <div class="navbar-brand">
+                    <h1 class="mt-3 has-text-white ml-5">Connect with me!</h1>
+
+
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="true"
+                        data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-end">
+                        <a class="navbar-item has-text-white" href="https://github.com/ArjayCaluag">
+                            Github
+                        </a>
+
+                        <a class="navbar-item has-text-white"
+                            href="https://www.linkedin.com/in/ron-arjay-caluag-00b29b182/">
+                            Linkedin
+                        </a>
+                        <a class="navbar-item has-text-white" href="assets/Ron-ArjayCaluagBerResume.pdf">
+                            Resume
+                        </a>
+                    </div>
+                </div>
+            </nav>
+  );
+}
+
+export default NavBar;
